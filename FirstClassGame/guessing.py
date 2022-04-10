56d7e1969af174df5a8376c25bb91c8885322494
JEsidio <jenifer_esidio@hotmail.com>
2022-04-10 02:11:25 -0300

    Added the while logic, count of tries and string formatation

--- FirstClassGame/guessing.py
+++ FirstClassGame/guessing.py
@@ -4,21 +4,35 @@ def game_start():
 
     print("***************************************");
     print("**** Welcome to the guessing game! ****");
-    print("***************************************");
+    print("***************************************\n");
 
+    number_of_tries = 0;
+
+    while not (number_of_tries in range(1, 10)):
+        number_of_tries = int(input("\nHow many tries would you like to have? (Type a number between 1 and 10): "))
+
+    current_try = 1;
     secret_number = 42;
-    guess = int(input("Type your guess number: "));
-    right_guess = (guess == secret_number);
-    higher_guess = (guess > secret_number);
 
-    print("\nYour guess was: ", guess);
+    print("\nNumber of tries: {}. Good luck!\n".format(number_of_tries));
 
-    if (right_guess):
-        print("Congratulations! You won!");
-    else:
-        if (higher_guess):
-            print("Sorry. You missed! Your guess was higher than the secret number.")
+    while (current_try <= number_of_tries):
+        print("Try: {} of {}".format(current_try, number_of_tries));
+        guess = int(input("Type your guess number: "));
+        right_guess = (guess == secret_number);
+        higher_guess = (guess > secret_number);
+
+        if (right_guess):
+            print("\nCongratulations! You won!\n");
+            break;
         else:
-            print("Sorry. You missed! Your guess was lower than the secret number.")
+            if (higher_guess):
+                print("\nSorry. You missed! Your guess was higher than the secret number.\n")
+            else:
+                print("\nSorry. You missed! Your guess was lower than the secret number.\n")
 
-    print("\nEnd of the game");
+        current_try = current_try + 1;
+
+    print("\n****************************************");
+    print("*********** End of the game! ***********");
+    print("****************************************");
