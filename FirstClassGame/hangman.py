e1f9d4419b901ed48f536695fcede319563513de
JEsidio <jenifer_esidio@hotmail.com>
2022-04-14 00:38:44 -0300

    Starting the logic for the hangman game

--- FirstClassGame/hangman.py
+++ FirstClassGame/hangman.py
@@ -1,5 +1,32 @@
 def start():
-    print("Hangman");
+    print("***************************************");
+    print("******* Welcome to the Hangman! *******");
+    print("***************************************\n");
+
+    secret_world = "banana";
+    right_words = ["_", "_", "_", "_", "_", "_"];
+    hanged = False;
+    won = False;
+
+    print(right_words)
+
+    while (not hanged and not won):
+
+        guess = input("Letter?");
+        guess = guess.strip();
+        index = 0;
+
+        for letter in secret_world:
+            if (guess.upper() == letter.upper()):
+                right_words[index] = letter;
+            index = index + 1;
+
+        print(right_words)
+
+
+    print("\n****************************************");
+    print("*********** End of the game! ***********");
+    print("****************************************");
 
 if (__name__ == "__main__"):
     start();
